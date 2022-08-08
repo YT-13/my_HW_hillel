e0f937323302a5fcea54d3c35ef7405d361f4568
YT-13 <108871808+YT-13@users.noreply.github.com>
2022-08-08 13:03:49 +0300

    Виконання ДЗ 1

---
+++ my_hw1.py
@@ -0,0 +1,11 @@
+# import this - iмпортує набір із 19 правил написання комп’ютерних програм, які впливають
+# дизайн мови програмування Python (The Zen of Python).
+import this
+
+# mport antigravity - відкриває посилання на сторінку з жартом чи коміксом: https://xkcd.com/353/
+import antigravity
+
+#import __hello__ - виводить на екран текст "Hello world!"
+import __hello__
+
+#from __future__ import braces - показує помилку "SyntaxError: not a chance"
